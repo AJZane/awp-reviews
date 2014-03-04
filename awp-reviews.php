c63a0e9767cf4593ac9d8092ce46da288affd0ac
AJ Zane <androiddreams@ajzane.com>
2014-03-03 19:59:24 -0800

    initial commit

---
+++ awp-reviews.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: AWP Plugin and Theme Reviews
+Plugin URI: https://github.com/AJZane/awp-reviews
+Description: A plugin and theme review plugin for the Advanced WordPress group
+Version: 0.1
+Author: Advanced WP
+Author URI: http://AdvancedWP.org
+License: GPL
+*/
+
+
+?>
