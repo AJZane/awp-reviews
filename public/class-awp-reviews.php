3386ee70e01ed83cb0cb1d3acdacf88474c81054
Brandon Lavigne <brandon.lavigne@gmail.com>
2014-03-10 11:33:00 -0700

    first pass at cpt

--- public/class-awp-reviews.php
+++ public/class-awp-reviews.php
@@ -65,6 +65,7 @@ class AWP_Reviews {
 
 		// Load plugin text domain
 		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+		add_action( 'init', array( $this, 'register_post_type') );
 
 		// Activate plugin when new blog is added
 		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
@@ -301,6 +302,92 @@ class AWP_Reviews {
 	 */
 	public function filter_method_name() {
 		// @TODO: Define your filter hook callback here
+	}
+
+	private function register_post_type(){
+
+		$labels = $this->post_type_labels( 'review' );
+	    $args = array(
+	      'public' => true,
+	      'labels'  => $labels
+	    );
+	    register_post_type( 'review', $args );
+	}
+
+	public function post_type_labels( $label="" ){
+		$plural = ucwords( pluralize($label) );
+		$single = ucwords($label);
+		$labels = array(
+		'name' => $plural,
+		'singular_name' => $single,
+		'add_new' => 'Add New',
+		'add_new_item' => 'Add New '.$single,
+		'edit_item' => 'Edit '.$single,
+		'new_item' => 'New '.$single,
+		'all_items' => 'All '.$plural,
+		'view_item' => 'View '.$single,
+		'search_items' => 'Search '.$plural,
+		'not_found' =>  'No '.$plural.' found',
+		'not_found_in_trash' => 'No '.$plural.' found in Trash', 
+		'parent_item_colon' => '',
+		'menu_name' => $plural
+		);
+		return $labels;
+	}
+
+	public function pluralize( $word ) {
+		$plural = array(
+			'/(quiz)$/i'                => '\1zes',
+			'/^(ox)$/i'                 => '\1en',
+			'/([m|l])ouse$/i'           => '\1ice',
+			'/(matr|vert|ind)ix|ex$/i'  => '\1ices',
+			'/(x|ch|ss|sh)$/i'          => '\1es',
+			'/([^aeiouy]|qu)ies$/i'     => '\1y',
+			'/([^aeiouy]|qu)y$/i'       => '\1ies',
+			'/(hive)$/i'                => '\1s',
+			'/(?:([^f])fe|([lr])f)$/i'  => '\1\2ves',
+			'/sis$/i'                   => 'ses',
+			'/([ti])um$/i'              => '\1a',
+			'/(buffal|tomat)o$/i'       => '\1oes',
+			'/(bu)s$/i'                 => '1ses',
+			'/(alias|status)/i'         => '\1es',
+			'/(octop|vir)us$/i'         => '1i',
+			'/(ax|test)is$/i'           => '\1es',
+			'/s$/i'                     => 's',
+			'/$/'                       => 's'
+		);
+
+		$uncountable = array( 'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep' );
+
+		$irregular = array(
+			'person'    => 'people',
+			'man'       => 'men',
+			'woman'     => 'women',
+			'child'     => 'children',
+			'sex'       => 'sexes',
+			'move'      => 'moves'
+		);
+
+		$lowercased_word = strtolower( $word );
+
+		foreach ( $uncountable as $_uncountable ) {
+			if ( substr( $lowercased_word, ( -1 * strlen( $_uncountable ) ) ) == $_uncountable ) {
+				return $word;
+			}
+		}
+
+		foreach ( $irregular as $_plural=> $_singular ) {
+			if ( preg_match( '/('.$_plural.')$/i', $word, $arr ) ) {
+				return preg_replace( '/('.$_plural.')$/i', substr( $arr[0], 0, 1 ).substr( $_singular, 1 ), $word );
+			}
+		}
+
+		foreach ( $plural as $rule => $replacement ) {
+			if ( preg_match( $rule, $word ) ) {
+				return preg_replace( $rule, $replacement, $word );
+			}
+		}
+		return false;
 	}
 
 }
